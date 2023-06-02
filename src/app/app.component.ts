eef216063df7c8474e2c3c67a190cd2f65be7d7b
patilmayur96 <87111155+patilmayur96@users.noreply.github.com>
2023-06-02 17:01:25 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
 import { Component } from '@angular/core';
 
+interface Country {
+  shortName: string;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-app';
+  
+  countries = ['USA', 'Canada', 'Australia'];
+  states: any = {
+    'USA': ['New York', 'California', 'Texas'],
+    'Canada': ['Ontario', 'Quebec', 'Alberta'],
+    'Australia': ['New South Wales', 'Victoria', 'Queensland']
+  };
+  cities: any = {
+    'New York': ['New York City', 'Buffalo', 'Rochester'],
+    'California': ['Los Angeles', 'San Francisco', 'San Diego'],
+    'Texas': ['Houston', 'Dallas', 'Austin'],
+    'Ontario': ['Toronto', 'Ottawa', 'Hamilton'],
+    'Quebec': ['Montreal', 'Quebec City', 'Sherbrooke'],
+    'Alberta': ['Calgary', 'Edmonton', 'Red Deer'],
+    'New South Wales': ['Sydney', 'Newcastle', 'Wollongong'],
+    'Victoria': ['Melbourne', 'Geelong', 'Ballarat'],
+    'Queensland': ['Brisbane', 'Gold Coast', 'Cairns']
+  };
+  
+  selectedCountry: string = '';
+  selectedState: string = '';
+  selectedCity: string = '';
+  selectedValues: string = '';
+  
+  onCountryChange() {
+    this.selectedState = '';
+    this.selectedCity = '';
+    console.log(this.selectedCountry)
+  }
+
+  
+  onStateChange() {
+    this.selectedCity = '';
+  }
+  
+  onCityChange() {
+    this.selectedValues = this.selectedCountry + ', ' + this.selectedState + ', ' + this.selectedCity;
+  }
+
+  resetValue(){
+    this.selectedCountry = '';
+    this.selectedState = '';
+    this.selectedCity = ''
+    this.statesArray = [];
+    this.citiesArray = []
+  }
+  
+  statesArray = [];
+  getStates() {
+    const selectedCountries = Array.isArray(this.selectedCountry) ? this.selectedCountry : [this.selectedCountry];
+  
+    for (let country of selectedCountries) {
+      this.statesArray = this.statesArray.concat(this.states[country]);
+    }
+  
+    return [...new Set(this.statesArray)];
+  }
+
+  citiesArray = []
+  getCities() {
+    const selectedStates = Array.isArray(this.selectedState) ? this.selectedState : [this.selectedState];
+  
+    for (let state of selectedStates) {
+      this.citiesArray = this.citiesArray.concat(this.cities[state]);
+    }
+  
+    return [...new Set(this.citiesArray)];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
